82f39a915a807cff5bb2df0bcdfb40d6fde64ab6
hovakimyan <liovaswagg@hotmail.com>
2024-12-02 16:57:50 +0100

    Modifs mineures

--- src/Faq.jsx
+++ src/Faq.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 
 const Faq = () => {
-    console.log('c\'est ok');
-    const [isResponseDisplayed, setIsResponseDelayed] = useState(false);
+    const [isResponseDisplayed, setIsResponseDisplayed] = useState(false);
 
     const displayResponse = () => {
-        setIsResponseDelayed(true);
+        console.log('Question cliquée !');
+        setIsResponseDisplayed(true);
     };
 
     return (
         <section>
             <p onClick={displayResponse}>Comment sont calculés les frais de port ?</p>
-
             {isResponseDisplayed && <p>Les frais de port sont calculés en fonction du poids de votre commande.</p>}
         </section>
     );
